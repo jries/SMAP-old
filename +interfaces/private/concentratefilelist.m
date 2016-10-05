df8e5aa7d4de20dbcc3a0f1730c01ae94e7b9342
Jonas <not@revealed.de>
2016-10-05 19:10:44 +0200

    save small filelist modified

--- +interfaces/private/concentratefilelist.m
+++ +interfaces/private/concentratefilelist.m
@@ -2,7 +2,7 @@ function loc=concentratefilelist(loc)
 %file
 for k=1:length(loc.file)
     try
-    loc.file.info.allmetadata.files=mycell2stringarray(loc.file.info.allmetadata.files);
+    loc.file.info.allmetadata.files=char(loc.file.info.allmetadata.files');
     catch
          disp('files')
     end
@@ -10,15 +10,20 @@ end
 %history
 for k=1:length(loc.history)
     try
-        loc.history(1).children.CameraConverter.loc_cameraSettings.allmetadata.files=mycell2stringarray(loc.history(1).children.CameraConverter.loc_cameraSettings.allmetadata.files);
+        loc.history{k}.children.CameraConverter.loc_cameraSettings.allmetadata.files=char(loc.history{k}.children.CameraConverter.loc_cameraSettings.allmetadata.files');
     catch
-        disp('history')
+        
+        try 
+            loc.history(k).children.CameraConverter.loc_cameraSettings.allmetadata.files=char(loc.history(k).children.CameraConverter.loc_cameraSettings.allmetadata.files');
+        catch
+            disp('history')
+        end
     end
 end
 
 for k=1:length(loc.siteexplorer.files)
     try
-        loc.siteexplorer.files(k).info.allmetadata.files=mycell2stringarray(loc.siteexplorer.files(k).info.allmetadata.files);
+        loc.siteexplorer.files(k).info.allmetadata.files=char(loc.siteexplorer.files(k).info.allmetadata.files');
     catch
          disp('se')
     end
--- shared/myfunctions/mycell2stringarray.m
+++
@@ -1,7 +0,0 @@
-function sout=mycell2stringarray(cin)
-len=max(cellfun(@length,cin));
-sout(length(cin),len)='a';
-for k=1:length(cin)
-    sout(k,1:length(cin{k}))=cin{k};
-end
-end
