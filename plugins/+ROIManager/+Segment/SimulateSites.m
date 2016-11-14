b1eccd68b3846513eb52606966a0318f63d18976
ries <jonas.ries@gmail.com>
2016-11-14 18:56:06 +0100

    simulator with persistence over many frames

--- plugins/+ROIManager/+Segment/SimulateSites.m
+++ plugins/+ROIManager/+Segment/SimulateSites.m
@@ -9,11 +9,12 @@ classdef SimulateSites<interfaces.DialogProcessor&interfaces.SEProcessor
         end
         
         function out=run(obj,p)  
-           [poslabels,possites]=getlabels(obj,p);
-           posreappear=reappear(poslabels,p.blinks);
-           p.maxframe=100000;
-           locs=locsfrompos(posreappear,p);
-           locst=singlelocs(locs);
+            [locst,possites]=simulatelocs(p);
+%            [poslabels,possites]=getlabels(obj,p);
+%            posreappear=reappear(poslabels,p.blinks);
+%            p.maxframe=100000;
+%            locs=locsfrompos(posreappear,p);
+%            locst=singlelocs(locs);
 %            figure(88);plot(locst.xnm,locst.ynm,'+',locst.xnm_gt,locst.ynm_gt,'o')
            obj.locData.addfile(['simulated_' num2str(obj.locData.files.filenumberEnd)]);
            obj.locData.addLocData(locst);
@@ -36,15 +37,15 @@ classdef SimulateSites<interfaces.DialogProcessor&interfaces.SEProcessor
         %         thissite.number=sitepar.sitelist.cellnumber+1;
                 se.addSite(thissite);
            end
-           try
+%            try
            se.processors.preview.updateFilelist;
            se.processors.preview.updateCelllist;
            se.processors.preview.updateSitelist; 
-           se.currentsite=se.site(1);
+           se.currentsite=se.sites(1);
            se.currentcell=se.cells(1);
            se.currentfile=se.files(1);
-           catch
-           end
+%            catch
+%            end
           out=[];
         end
         function pard=guidef(obj)
@@ -53,30 +54,30 @@ classdef SimulateSites<interfaces.DialogProcessor&interfaces.SEProcessor
     end
 end
 
-function locso=singlelocs(locs)
-numl=0;
-fn=fieldnames(locs(1));
-for k=1:length(locs)
-    numl=numl+length(locs(k).(fn{1}));
-end
+% function locso=singlelocs(locs)
+% numl=0;
+% fn=fieldnames(locs(1));
+% for k=1:length(locs)
+%     numl=numl+length(locs(k).(fn{1}));
+% end
+% 
+% % locso=locs(1);
+% % locso.(fn{1})(1)=0;
+% for k=1:length(fn)
+%     locso.(fn{k})(numl,1)=locs(1).(fn{k})(1); %initialize
+% end
+% 
+% ind=1;
+% for l=1:length(locs)
+%     numlh=length(locs(l).(fn{1}));
+%     for k=1:length(fn)
+%         locso.(fn{k})(ind:ind+numlh-1)=locs(l).(fn{k});
+%     end
+%     ind=ind+numlh;
+% end
+% end
 
-% locso=locs(1);
-% locso.(fn{1})(1)=0;
-for k=1:length(fn)
-    locso.(fn{k})(numl,1)=locs(1).(fn{k})(1); %initialize
-end
 
-ind=1;
-for l=1:length(locs)
-    numlh=length(locs(l).(fn{1}));
-    for k=1:length(fn)
-        locso.(fn{k})(ind:ind+numlh-1)=locs(l).(fn{k});
-    end
-    ind=ind+numlh;
-end
-
-
-end
 function load_callback(a,b,obj)
 f=obj.getSingleGuiParameter('coordinatefile');
 [f,p]=uigetfile({'*.*';'*.tif';'*.png';'*.csv';'*.txt';'*.mat'},'Choose coordinate file',f);
@@ -124,190 +125,190 @@ obj.guihandles.tif_imagesizet.Visible=tif;
 obj.guihandles.tif_imagesize.Visible=tif;
 end
 
-function [locs,possites]=getlabels(obj,p)
-[~,~,ext]=fileparts(p.coordinatefile);
-image=[];
-locsall=[];
-switch ext
-    case {'.txt','.csv'}
-        plocs=readtable(p.coordinatefile);
-        plocsa=table2array(plocs);
-        locsall.x=plocsa(:,1);
-        locsall.y=plocsa(:,2);
-        if size(plocsa,2)>2
-            locsall.z=plocsa(:,3);
-        end
-        locsall=copyfields(locsall,plocs,{'x','y','z'});
-    case {'.tif','.png'}
-%         locs=getlabelstiff(obj,p);
-        image=imread(p.coordinatefile);
-        img=sum(image,3)/size(image,3); %binarize
-        image=double(img)/255;
-        
-    case '.mat'
-        l=load(p.coordinatefile);
-        
-        if isfield(l,'image')
-            image=l.image;
-        else
-            locsall=copyfields([],l,{'x','y','z'});
-        end
-    case '.m'
-        cf=pwd;
-        [ph,fh]=fileparts(p.coordinatefile);
-        cd(ph)
-        l=eval(fh);
-        cd(cf);
-%         l=eval(p.coordinatefile);
-        if isfield(l,'image')
-            image=l.image;
-        else
-            locsall=copyfields([],l,{'x','y','z'});
-        end
-    otherwise
-        display('file not identified selected')
-        return
-end
-
-distsites=p.se_sitefov;
-if length(p.numberofsites)>1
-    numberofrows=p.numberofsites(2);
-    numberofsites=p.numberofsites(1)*p.numberofsites(2);
-else
-    numberofrows=ceil(32000/p.se_sitefov);
-    numberofsites=p.numberofsites;
-end
-
-% numeroflines=ceil(p.numberofsites/numberofrows);
-for k=numberofsites:-1:1
-    xh=mod(k-1,numberofrows);
-    yh=ceil(k/numberofrows);
-    if ~isempty(image)
-        locsh=locsfromimage(image,p);
-    else
-        locsh=labelremove(locsall,p.labeling_efficiency);
-    end
-
-    numlocs=length(locsh.x);
-    locsh.x=reshape(locsh.x,numlocs,1);
-    locsh.y=reshape(locsh.y,numlocs,1);
-    if p.randomrot
-        angle=2*pi*rand(1);
-        [locsh.x,locsh.y]=rotcoord(locsh.x,locsh.y,angle);
-    else
-        angle=0;
-    end
-    
-    if p.randomxy 
-        dx=(rand(1)-.5)*p.randomxyd*2;dy=(rand(1)-.5)*p.randomxyd*2;
-        locsh.x=locsh.x+dx;
-        locsh.y=locsh.y+dy;
-    else
-        dx=0;
-        dy=0;
-    end
-    
-    locs(k).x=locsh.x+xh*distsites;
-    locs(k).y=locsh.y+yh*distsites;
-    locs(k).angle=angle*ones(size(locsh.x));
-    locs(k).dx_gt=dx*ones(size(locsh.x));
-    locs(k).dy_gt=dy*ones(size(locsh.x));
-        
-    possites(k).x=xh*distsites;
-    possites(k).y=yh*distsites;
-%     figure(89);plot(locs(k).x,locs(k).y,'*')
-%     waitforbuttonpress
-    
-end
-end
-
-
-function locs=locsfromimage(image,p)
-
-if p.tif_numbermode.Value==1
-    density=p.tif_density;
-else
-    %calcualte density from number of locs
-%     pdensity=mean(image(:))/(p.tif_imagesize/1000)^2;
-    density=p.tif_density/mean(image(:))/(p.tif_imagesize/1000)^2;
-end
-numtot=round(density*(p.tif_imagesize/1000)^2);
-
-x=rand(numtot,1);
-y=rand(numtot,1);
-
-
-xpix=ceil(x*size(image,1));
-ypix=ceil(y*size(image,2));
-linind=sub2ind(size(image),xpix,ypix);
-keep=image(linind)>rand(numtot,1);
-
-locs.x=(x(keep)-0.5)*p.tif_imagesize;
-locs.y=(y(keep)-0.5)*p.tif_imagesize;
-% pixf=size(image,1)/(p.tif_imagesize/1000);
-% densitypixel=density/pixf^2;
-
-end
-
-function locs=labelremove(locin,p)
-numl=length(locin.x);
-indin=rand(numl,1)<=p;
-locs=copystructReduce(locin,indin);
-end
-
-function locso=reappear(locs,numblinks)
-for k=length(locs):-1:1
-    locso(k)=reappeari(locs(k),numblinks);
-end
-end
-function locso=reappeari(locs,numblinks)
-fn=fieldnames(locs);
-numlocs=length(locs.(fn{1}));
-numn=round(exprnd(numblinks,numlocs,1));
-indextra=zeros(sum(numn),1);
-idx=1;
-for k=1:length(numn)
-    indextra(idx:idx+numn(k)-1)=k;
-    idx=idx+numn(k);
-end
-indout=[(1:numlocs)'; indextra];
-for k=1:length(fn)
-    locso.(fn{k})=locs.(fn{k})(indout);
-end
-end
-
-
-function locs=locsfrompos(locsi,p)
-for k=length(locsi):-1:1
-    locs(k)=locsfromposi(locsi(k),p);
-end
-end
-
-function locs=locsfromposi(locsi,p)
-    numlocs=length(locsi.x);
-    phot=exprnd(p.photons,numlocs,1);
-    
-    a=100;
-    PSF=100;
-    sa=PSF+a/12;
-    phot(phot<10)=10;
-    indin=phot>=10;
-    numlocs=sum(indin);
-    %MOrtensen
-    locprecnm=sqrt(sa^2./phot.*(16/9+8*pi*sa^2*p.background^2./phot/a^2));
-    locs.phot=single(phot(indin));
-    locs.bg=single(locprecnm(indin)*0+p.background);
-    locs.locprecnm=single(locprecnm(indin));
-    locs.frame=double(ceil(rand(numlocs,1)*p.maxframe));
-    locs.xnm=single(locsi.x(indin)+randn(numlocs,1).*locprecnm(indin));
-    locs.ynm=single(locsi.y(indin)+randn(numlocs,1).*locprecnm(indin));
-    locs.xnm_gt=single(locsi.x(indin));
-    locs.ynm_gt=single(locsi.y(indin));
-    locs.dxnm_gt=single(locsi.dx_gt(indin));
-    locs.dynm_gt=single(locsi.dy_gt(indin));
-    locs.angle=single(locsi.angle(indin));
-      
-end
+% function [locs,possites]=getlabels(obj,p)
+% [~,~,ext]=fileparts(p.coordinatefile);
+% image=[];
+% locsall=[];
+% switch ext
+%     case {'.txt','.csv'}
+%         plocs=readtable(p.coordinatefile);
+%         plocsa=table2array(plocs);
+%         locsall.x=plocsa(:,1);
+%         locsall.y=plocsa(:,2);
+%         if size(plocsa,2)>2
+%             locsall.z=plocsa(:,3);
+%         end
+%         locsall=copyfields(locsall,plocs,{'x','y','z'});
+%     case {'.tif','.png'}
+% %         locs=getlabelstiff(obj,p);
+%         image=imread(p.coordinatefile);
+%         img=sum(image,3)/size(image,3); %binarize
+%         image=double(img)/255;
+%         
+%     case '.mat'
+%         l=load(p.coordinatefile);
+%         
+%         if isfield(l,'image')
+%             image=l.image;
+%         else
+%             locsall=copyfields([],l,{'x','y','z'});
+%         end
+%     case '.m'
+%         cf=pwd;
+%         [ph,fh]=fileparts(p.coordinatefile);
+%         cd(ph)
+%         l=eval(fh);
+%         cd(cf);
+% %         l=eval(p.coordinatefile);
+%         if isfield(l,'image')
+%             image=l.image;
+%         else
+%             locsall=copyfields([],l,{'x','y','z'});
+%         end
+%     otherwise
+%         display('file not identified selected')
+%         return
+% end
+% 
+% distsites=p.se_sitefov;
+% if length(p.numberofsites)>1
+%     numberofrows=p.numberofsites(2);
+%     numberofsites=p.numberofsites(1)*p.numberofsites(2);
+% else
+%     numberofrows=ceil(32000/p.se_sitefov);
+%     numberofsites=p.numberofsites;
+% end
+% 
+% % numeroflines=ceil(p.numberofsites/numberofrows);
+% for k=numberofsites:-1:1
+%     xh=mod(k-1,numberofrows);
+%     yh=ceil(k/numberofrows);
+%     if ~isempty(image)
+%         locsh=locsfromimage(image,p);
+%     else
+%         locsh=labelremove(locsall,p.labeling_efficiency);
+%     end
+% 
+%     numlocs=length(locsh.x);
+%     locsh.x=reshape(locsh.x,numlocs,1);
+%     locsh.y=reshape(locsh.y,numlocs,1);
+%     if p.randomrot
+%         angle=2*pi*rand(1);
+%         [locsh.x,locsh.y]=rotcoord(locsh.x,locsh.y,angle);
+%     else
+%         angle=0;
+%     end
+%     
+%     if p.randomxy 
+%         dx=(rand(1)-.5)*p.randomxyd*2;dy=(rand(1)-.5)*p.randomxyd*2;
+%         locsh.x=locsh.x+dx;
+%         locsh.y=locsh.y+dy;
+%     else
+%         dx=0;
+%         dy=0;
+%     end
+%     
+%     locs(k).x=locsh.x+xh*distsites;
+%     locs(k).y=locsh.y+yh*distsites;
+%     locs(k).angle=angle*ones(size(locsh.x));
+%     locs(k).dx_gt=dx*ones(size(locsh.x));
+%     locs(k).dy_gt=dy*ones(size(locsh.x));
+%         
+%     possites(k).x=xh*distsites;
+%     possites(k).y=yh*distsites;
+% %     figure(89);plot(locs(k).x,locs(k).y,'*')
+% %     waitforbuttonpress
+%     
+% end
+% end
+% 
+% 
+% function locs=locsfromimage(image,p)
+% 
+% if p.tif_numbermode.Value==1
+%     density=p.tif_density;
+% else
+%     %calcualte density from number of locs
+% %     pdensity=mean(image(:))/(p.tif_imagesize/1000)^2;
+%     density=p.tif_density/mean(image(:))/(p.tif_imagesize/1000)^2;
+% end
+% numtot=round(density*(p.tif_imagesize/1000)^2);
+% 
+% x=rand(numtot,1);
+% y=rand(numtot,1);
+% 
+% 
+% xpix=ceil(x*size(image,1));
+% ypix=ceil(y*size(image,2));
+% linind=sub2ind(size(image),xpix,ypix);
+% keep=image(linind)>rand(numtot,1);
+% 
+% locs.x=(x(keep)-0.5)*p.tif_imagesize;
+% locs.y=(y(keep)-0.5)*p.tif_imagesize;
+% % pixf=size(image,1)/(p.tif_imagesize/1000);
+% % densitypixel=density/pixf^2;
+% 
+% end
+% 
+% function locs=labelremove(locin,p)
+% numl=length(locin.x);
+% indin=rand(numl,1)<=p;
+% locs=copystructReduce(locin,indin);
+% end
+% 
+% function locso=reappear(locs,numblinks)
+% for k=length(locs):-1:1
+%     locso(k)=reappeari(locs(k),numblinks);
+% end
+% end
+% function locso=reappeari(locs,numblinks)
+% fn=fieldnames(locs);
+% numlocs=length(locs.(fn{1}));
+% numn=round(exprnd(numblinks,numlocs,1));
+% indextra=zeros(sum(numn),1);
+% idx=1;
+% for k=1:length(numn)
+%     indextra(idx:idx+numn(k)-1)=k;
+%     idx=idx+numn(k);
+% end
+% indout=[(1:numlocs)'; indextra];
+% for k=1:length(fn)
+%     locso.(fn{k})=locs.(fn{k})(indout);
+% end
+% end
+% 
+% 
+% function locs=locsfrompos(locsi,p)
+% for k=length(locsi):-1:1
+%     locs(k)=locsfromposi(locsi(k),p);
+% end
+% end
+% 
+% function locs=locsfromposi(locsi,p)
+%     numlocs=length(locsi.x);
+%     phot=exprnd(p.photons,numlocs,1);
+%     
+%     a=100;
+%     PSF=100;
+%     sa=PSF+a/12;
+%     phot(phot<10)=10;
+%     indin=phot>=10;
+%     numlocs=sum(indin);
+%     %MOrtensen
+%     locprecnm=sqrt(sa^2./phot.*(16/9+8*pi*sa^2*p.background^2./phot/a^2));
+%     locs.phot=single(phot(indin));
+%     locs.bg=single(locprecnm(indin)*0+p.background);
+%     locs.locprecnm=single(locprecnm(indin));
+%     locs.frame=double(ceil(rand(numlocs,1)*p.maxframe));
+%     locs.xnm=single(locsi.x(indin)+randn(numlocs,1).*locprecnm(indin));
+%     locs.ynm=single(locsi.y(indin)+randn(numlocs,1).*locprecnm(indin));
+%     locs.xnm_gt=single(locsi.x(indin));
+%     locs.ynm_gt=single(locsi.y(indin));
+%     locs.dxnm_gt=single(locsi.dx_gt(indin));
+%     locs.dynm_gt=single(locsi.dy_gt(indin));
+%     locs.angle=single(locsi.angle(indin));
+%       
+% end
 function pard=guidef(obj)
 
 % 
@@ -350,13 +351,14 @@ pard.blinks.object=struct('String','1','Style','edit');
 pard.blinks.Width=.5;
 pard.blinks.position=[4,2.5];
 
-% pard.t3.object=struct('String','lifetime (frames)','Style','text');
-% pard.t3.position=[4,3];
-% pard.t3.Width=1.5;
 
-% pard.lifetime.object=struct('String','1','Style','edit');
-% pard.lifetime.Width=.5;
-% pard.lifetime.position=[4,4.5];
+pard.t3.object=struct('String','lifetime (frames)','Style','text');
+pard.t3.position=[4,3];
+pard.t3.Width=1.5;
+
+pard.lifetime.object=struct('String','1','Style','edit');
+pard.lifetime.Width=.5;
+pard.lifetime.position=[4,4.5];
 
 pard.t4.object=struct('String','mean number photons','Style','text');
 pard.t4.position=[5,1];
