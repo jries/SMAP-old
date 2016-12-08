5e5cf3ff39f48848c80b02866202208fadd39259
ries <jonas.ries@gmail.com>
2016-12-08 11:35:47 +0100

    compare ground truth

--- plugins/+Analyze/+other/CompareToGroundTruth.m
+++ plugins/+Analyze/+other/CompareToGroundTruth.m
@@ -7,7 +7,32 @@ classdef CompareToGroundTruth<interfaces.DialogProcessor
         end
         
         function out=run(obj,p)
-           
+            if p.checklayer
+                % do something with grouping? at least warn?
+                lr=obj.locData.getloc({'xnm','ynm','znm','phot'},'layer',p.reflayer.Value);
+                lt=obj.locData.getloc({'xnm','ynm','znm','phot'},'layer',p.targetlayer.Value);
+                [outlayer2D, outlayer3D]=getmatch(lr,lt,p);
+                outlayer2D.name='layer2D';
+                outlayer3D.name='layer3D';
+            end
+            if p.checkdata
+            end
+            tab=maketable(outlayer2D,outlayer3D);
+            ax=obj.initaxis('results');
+            f=ax.Parent;
+            delete(ax);
+%             pos=f.Position;pos(1:2)=20;pos(3:4)=pos(3:4)*.9;
+            uit=uitable(f,'Units','normalized','Position',[0 0 1 1]);
+            uit.Units='pixels';
+            uit.Data=table2cell(tab);
+            uit.ColumnName=tab.Properties.VariableNames;
+            ncol=length(uit.ColumnName);
+            w=uit.Position(3)/(ncol+1);
+            for k=1:ncol
+              cw{k}=w;
+            end
+            uit.ColumnWidth=cw;
+            out=[];
         end
         function pard=guidef(obj)
             pard=guidef(obj);
@@ -15,7 +40,62 @@ classdef CompareToGroundTruth<interfaces.DialogProcessor
     end
 end
 
+function [out,out3D]=getmatch(lr,lt,p)
+if length(p.searchradius)==1
+    p.searchradius(2)=p.searchradius(1);
+end
+    [mr,mt,ur,ut]=matchlocs(lr.xnm,lr.ynm,lt.xnm,lt.ynm,[],p.searchradius(1));
+     out=getmatchstat(lr,lt,mr,mt,ur,ut);  
+    
+    %look at z
+    if isfield(lr,'znm')&&~isempty(lr.znm)&&isfield(lt,'znm')&&~isempty(lt.znm)
+        dz=lr.znm(mr)-lt.znm(mt);
+        inz=abs(dz)<=p.searchradius(2);
+        ur2=vertcat(ur,mr(~inz));
+        ut2=vertcat(ut,mt(~inz));
+        mr2=mr(inz);
+        mt2=mt(inz);
+    end
+    out3D=getmatchstat(lr,lt,mr2,mt2,ur2,ut2);  
+end
 
+function out=getmatchstat(lr,lt,mr,mt,ur,ut)
+    nrtot=length(mr)+length(ur);
+    nttot=length(mt)+length(ut);
+    out.falsePositives=length(ut)/nrtot;
+    out.falseNegatives=length(ur)/nrtot;
+    
+    dx=lr.xnm(mr)-lt.xnm(mt);
+    dy=lr.ynm(mr)-lt.ynm(mt);
+    if isfield(lr,'znm')&&~isempty(lr.znm)&&isfield(lt,'znm')&&~isempty(lt.znm)
+        dz=lr.znm(mr)-lt.znm(mt);
+    else
+        dz=0*dx;
+    end
+    out.shift=[mean(dx),mean(dy),mean(dz)];
+    out.stderr=[std(dx),std(dy),std(dz)];
+    out.err=sqrt([mean(dx.^2),mean(dy.^2),mean(dz.^2)]);
+    out.meanabs=([mean(abs(dx)),mean(abs(dy)),mean(abs(dz))]);
+
+end
+
+function tab=maketable(varargin)
+for k=1:length(varargin)
+    inh=varargin{k};
+    ss.name{k,1}=inh.name;
+    ss.fPos(k,1)=inh.falsePositives;
+    ss.fNeg(k,1)=inh.falseNegatives;
+    ss.Jaccard(k,1)=(1-inh.falseNegatives)*(1-inh.falsePositives);
+    ss.stdxy(k,1)=sqrt(sum(inh.stderr.^2));
+    ss.meanabsxy(k,1)=mean(inh.meanabs);
+    ss.errxy(k,1)=sqrt(sum(inh.err.^2));
+    ss.stdz(k,1)=(inh.stderr(3));
+    ss.meanabsz(k,1)=mean(inh.meanabs(3));
+    ss.errz(k,1)=(inh.err(3));
+end
+tab=struct2table(ss);
+
+end
 function wobble_callback(a,b,obj)
 filename=(obj.locData.files.file(1).name);
 path=fileparts(filename);
@@ -92,10 +172,10 @@ pard.t1.position=[1,1.5];
 pard.t2.object=struct('Style','text','String','Target');
 pard.t2.position=[1,2.5];
 
-pard.t3.object=struct('Style','text','String','search radius (nm)');
+pard.t3.object=struct('Style','text','String','search radius x,y (nm)');
 pard.t3.position=[1,3.5];
 pard.t3.Width=1;
-pard.searchradius.object=struct('Style','edit','String','100');
+pard.searchradius.object=struct('Style','edit','String','100 300');
 pard.searchradius.position=[1,4.5];
 pard.searchradius.Width=.5;
 % pard.onlyfiltered.object=struct('Style','checkbox','String','Export filtered (displayed) localizations.','Value',1);
