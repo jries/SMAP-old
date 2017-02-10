8e8465591190650ecdb1d41d901a521459630f58
ries <jonas.ries@gmail.com>
2017-02-10 12:18:12 +0100

    image slicer

--- shared/myfunctions/imageslicer.m
+++ shared/myfunctions/imageslicer.m
@@ -40,28 +40,32 @@ ax.XLim=[0 Inf];
 
 vp1=0.07;
 vp2=0.02;
-hs=uicontrol('Parent',phandle,'Style','slider','Units','normalized','Position',[0.05 vp1 0.35 0.05],...
+p
+hslider{1}=uicontrol('Parent',phandle,'Style','slider','Units','normalized','Position',[0.05 vp1 0.35 0.05],...
     'Min',1,'Max',size(V,3),'Value',1,'SliderStep',[1/(size(V,3)-1) 5/(size(V,3)-1)],'Callback',{@slidercallback,1});
-hs2=uicontrol('Parent',phandle,'Style','slider','Units','normalized','Position',[0.05 vp2 0.35 0.05],...
-    'Min',1,'Max',size(V,3),'Value',1,'SliderStep',[1/(size(V,3)-1) 5/(size(V,3)-1)],'Callback',{@slidercallback,1});
-hn=uicontrol('Parent',phandle,'Style','edit','Units','normalized','String','1','Position',[0.4 vp1 0.075 0.05],'Callback',{@framecallback,1});
-hn2=uicontrol('Parent',phandle,'Style','edit','Units','normalized','String','1','Position',[0.4 vp2 0.075 0.05],'Callback',{@framecallback,2});
+hslider{2}=uicontrol('Parent',phandle,'Style','slider','Units','normalized','Position',[0.05 vp2 0.35 0.05],...
+    'Min',1,'Max',size(V,3),'Value',1,'SliderStep',[1/(size(V,3)-1) 5/(size(V,3)-1)],'Callback',{@slidercallback,2});
+hframe{1}=uicontrol('Parent',phandle,'Style','edit','Units','normalized','String','1','Position',[0.4 vp1 0.075 0.05],'Callback',{@framecallback,1});
+hframe{2}=uicontrol('Parent',phandle,'Style','edit','Units','normalized','String','1','Position',[0.4 vp2 0.075 0.05],'Callback',{@framecallback,2});
 
-hmenux=uicontrol('Parent',phandle,'Style','popupmenu','Units','normalized','String',{'x','y','z'},'Position',[0.475 vp1 0.125 0.05],...
-    'Callback',@changeaxis);
-hmenuy=uicontrol('Parent',phandle,'Style','popupmenu','Units','normalized','String',{'x','y','z'},'Position',[0.6 vp1 0.125 0.05],...
-    'Callback',@changeaxis,'Value',2);
+hslidert{1}=uicontrol('Parent',phandle,'Style','text','Units','normalized','Position',[0.02 vp1 0.03 0.05],'String','3');
+hslidert{2}=uicontrol('Parent',phandle,'Style','text','Units','normalized','Position',[0.02 vp2 0.03 0.05],'String','4');
+
+hmenu{1}=uicontrol('Parent',phandle,'Style','popupmenu','Units','normalized','String',{'x','y','z'},'Position',[0.475 vp1 0.125 0.05],...
+    'Callback',{@changeaxis,0});
+hmenu{2}=uicontrol('Parent',phandle,'Style','popupmenu','Units','normalized','String',{'x','y','z'},'Position',[0.6 vp1 0.125 0.05],...
+    'Callback',{@changeaxis,1},'Value',2);
 haxscale=uicontrol('Parent',phandle,'Style','checkbox','Units','normalized','String','fill','Position',[0.475 vp2 0.1 0.05],...
-    'Callback',@plotimage,'Value',1);
+    'Callback',@plotimage,'Value',p.fill);
 hlut=uicontrol('Parent',phandle,'Style','popupmenu','Units','normalized','String',{'parula','gray','hot','jet'},'Position',[0.725 vp1 0.175 0.05],...
     'Callback',@plotimage);
 hcontrastcheck=uicontrol('Parent',phandle,'Style','checkbox','Units','normalized','String','global contrast','Position',[0.6 vp2 0.2 0.05],...
-    'Callback',@plotimage,'Value',0);
+    'Callback',@plotimage,'Value',p.globalcontrast);
 hcontrast=uicontrol('Parent',phandle,'Style','edit','Units','normalized','String','1','Position',[0.8 vp2 0.1 0.05],'Callback',@plotimage);
 
 hresetax=uicontrol('Parent',phandle,'Style','pushbutton','Units','normalized','String','reset','Position',[0.9 vp2 0.1 0.05],'Callback',@resetax);
 
-hrgb=uicontrol('Parent',phandle,'Style','checkbox','Units','normalized','String','RGB','Position',[0.9 vp1 0.1 0.05],'Callback',@updateall);
+hrgb=uicontrol('Parent',phandle,'Style','checkbox','Units','normalized','String','RGB','Position',[0.9 vp1 0.1 0.05],'Callback',@updateall,'Value',p.rgb);
 updateall
 
 plotimage
@@ -69,6 +73,7 @@ plotimage
 
     function resetax(a,b)
         ax.XLim=[0 Inf];
+        hcontrast.String='1';
         plotimage;
     end
     function slidercallback(a,b,slider)
@@ -78,25 +83,39 @@ plotimage
        setslice(str2double( a.String),slider);
     end
 
-    function changeaxis(a,b,ax)
-        switch a.String{a.Value}
-            case 'xy'
-                dim = [1 2 3];
-            case 'yz'
-                dim = [2 3 1];
-            case 'xz'
-                dim = [1 3 2];
+    function changeaxis(a,b,axv)
+        oax=~axv;
+        
+        
+        if strcmp(hmenu{axv+1}.String{hmenu{axv+1}.Value},hmenu{oax+1}.String{hmenu{oax+1}.Value})
+            if hmenu{axv+1}.Value==1
+                hmenu{oax+1}.Value=2;
+            else
+                hmenu{oax+1}.Value=1;
+            end
         end
-        ax.XLim=[0 Inf];
+        ax.XLim=[-inf inf];
+        ax.YLim=[-inf inf];
         updateall
+%         plotimage
+%         switch a.String{a.Value}
+%             case 'xy'
+%                 dim = [1 2 3];
+%             case 'yz'
+%                 dim = [2 3 1];
+%             case 'xz'
+%                 dim = [1 3 2];
+%         end
+%         ax.XLim=[0 Inf];
+%         updateall
 
     end
     
     function setslice(frame,slider)
-        frame=min(frame,hs.Max);
+        frame=min(frame,hslider{slider}.Max);
         frame=max(1,frame);
-        hn.String=num2str(round(frame));
-        hs.Value=round(frame);
+        hframe{slider}.String=num2str(round(frame));
+        hslider{slider}.Value=round(frame);
         plotimage
     end
     function updateall(a,b)
@@ -118,23 +137,49 @@ plotimage
         for k=1:length(dims)
             str{end+1}=num2str(dims(k));
         end
-        hmenux.String=str;
-        hmenuy.String=str;
-        
-        dim(1)=hmenux.Value;
-            dim(2)=hmenuy.Value; 
+        hmenu{1}.String=str;
+        hmenu{2}.String=str;
+         if hmenu{1}.Value>2 
+            dim(1)=str2double(str(hmenu{1}.Value));
+         else
+             dim(1)=hmenu{1}.Value;
+         end
+         if hmenu{2}.Value>2 
+            dim(2)=str2double(str(hmenu{2}.Value));
+         else
+             dim(2)=hmenu{2}.Value;
+         end
+%             dim(2)=str2double(str(hmenu{2}.Value)); 
         dimmenu=setdiff(1:length(s),[dim dimrgb]);
-            
-        hs.SliderStep=[1/(size(V,dimmenu(1))-1) 5/(size(V,dimmenu(1))-1)];
-        hs.Max=size(V,dimmenu(1));
+        
+%         strm=str;strm(dimrgb)=[];
+        hslider{1}.SliderStep=[1/(size(V,dimmenu(1))-1) 5/(size(V,dimmenu(1))-1)];
+        hslider{1}.Max=size(V,dimmenu(1));
+         setslice(min(hslider{1}.Max,hslider{1}.Value),1);
+         
+         if dimmenu(1)<3
+         hslidert{1}.String=str{dimmenu(1)};
+         else
+             hslidert{1}.String=num2str(dimmenu(1));
+         end
         if length(dimmenu)>1
-            hs2.SliderStep=[1/(size(V,dimmenu(2))-1) 5/(size(V,dimmenu(2))-1)];
-            hs2.Max=size(V,dimmenu(2));
-            setslice(min(hs.Max,hs.Value));
-            hs2.Visible='on';
+            hslider{2}.SliderStep=[1/(size(V,dimmenu(2))-1) 5/(size(V,dimmenu(2))-1)];
+            hslider{2}.Max=size(V,dimmenu(2));
+             setslice(min(hslider{2}.Max,hslider{2}.Value),2);
+            hslider{2}.Visible='on';
+            hframe{2}.Visible='on';
+            hslidert{2}.Visible='on';
+             if dimmenu(2)<3
+                hslidert{2}.String=str{dimmenu(2)};
+             else
+                 hslidert{2}.String=num2str(dimmenu(2));
+             end
         else
-            hs2.Visible='off';
+            hslider{2}.Visible='off';
+            hframe{2}.Visible='off';
+            hslidert{2}.Visible='off';
         end
+        plotimage
         
     end
     function plotimage(a,b,c)
@@ -146,8 +191,8 @@ plotimage
         xlimold=ax.XLim;
         ylimold=ax.YLim;
         
-%         slicez=round(str2double(hn.String));
-%         slicet=round(str2double(hn2.String));
+%         slicez=round(str2double(hframe.String));
+%         slicet=round(str2double(hframe2.String));
         
         if dim(1)<=3
             a1=axl{dim(1)};
@@ -157,15 +202,23 @@ plotimage
         if dim(2)<=3
             a2=axl{dim(2)};
         else
-            a1=1:size(V,dim(2));
+            a2=1:size(V,dim(2));
         end
-        dimall{dimmenu(1)}=round(str2double(hn.String));
+        dimall{dimmenu(1)}=round(str2double(hframe{1}.String));
         if length(dimmenu)>1
-            dimall{dimmenu(2)}=round(str2double(hn2.String));
+            dimall{dimmenu(2)}=round(str2double(hframe{2}.String));
+            dimmenu2=dimmenu(2);
+        else
+            dimmenu2=[];
         end
-        img=permute(squeeze(V(dimall{:})),[dim(1) dim(2) dimrgb]);
+        Vsl=V(dimall{:});
         
-%         switch hmenux.String{hmenux.Value}
+        
+        Vslp=permute(Vsl,[dim(2) dim(1) dimrgb dimmenu(1) dimmenu2]);
+        
+        img=(squeeze(Vslp));
+        
+%         switch hmenu{1}.String{hmenu{1}.Value}
 %             case 'xy'
 %                img=V(:,:,slice)';
 %                a1=x;a2=y;
@@ -185,26 +238,42 @@ plotimage
         end
         
         img(img>imax)=imax;
-        img(1,1)=imax;
+        
+        if length(size(img))==3
+            img=img/imax;
+        end
+%         img(1,1)=imax; %replace by scaling
         imagesc(ax,a1,a2,img);
+%         imagesc(ax,img);
         if haxscale.Value
             axis(ax,'fill')
         else
             axis(ax,'equal')
         end
         if ~isinf(xlimold(2))
-        ax.XLim=xlimold;ax.YLim=ylimold;
+            ax.XLim=xlimold;ax.YLim=ylimold;
+        else
+            d1=a1(2)-a1(1);
+            d2=a2(2)-a2(1);
+            ax.XLim=a1([1 end])+[-1 1]*d1/2;
+            ax.YLim=a2([1 end])+[-1 1]*d2/2;
         end
         colormap(ax,hlut.String{hlut.Value})
+        ax.CLim=[0 imax];
         
     end
     function keypress(a,b)
+        if contains(b.Modifier,'shift')
+            slider=2;
+        else
+            slider=1;
+        end
         if strcmp(b.Character,'+')||strcmp(b.Key,'rightarrow')
-            frame=hs.Value;
-            setslice(frame+1);
+            frame=hslider{slider}.Value;
+            setslice(frame+1,slider);
         elseif strcmp(b.Character,'-')||strcmp(b.Key,'leftarrow')
-            frame=hs.Value;
-            setslice(frame-1);
+            frame=hslider{slider}.Value;
+            setslice(frame-1,slider);
         elseif strcmp(b.Key,'uparrow')
             hcontrast.String=num2str(str2double(hcontrast.String)*1.1,'%1.2f');
             plotimage;
@@ -225,8 +294,11 @@ p.addOptional('y',[],@isnumeric);
 p.addOptional('z',[],@isnumeric);
 p.addRequired('V',@isnumeric);
 p.addParameter('Parent',[]);
-p.addParameter('fill',true,@islogical);
-p.addParameter('view',[],@(x) any(validatestring(x,{'xy','xz','yz'})));
+p.addParameter('fill',true);
+p.addParameter('xdim',[],@isnumeric);
+p.addParameter('ydim',[],@isnumeric);
+p.addParameter('rgb',false);
+p.addParameter('globalcontrast',false,@islogical);
 parse(p,in{:});
 pv=p.Results;
 
