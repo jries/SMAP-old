f97a8f501fdde0426317a3f51d6dcedb70bc90aa
Jonas <not@revealed.de>
2017-03-06 20:49:53 +0100

    asymmetry during fit

--- shared/asymmetry.m
+++ shared/asymmetry.m
@@ -1,31 +1,33 @@
-function [out,dir]=asymmetry(imin)
+function [out,asymmr,dir]=asymmetry(imin)
 if iscell(imin)
     for k=numel(imin):-1:1
-        [out{k},dir{k}]=asymmetryi(imin{k});
+        [out{k},asymmr{k},dir{k}]=asymmetryi(imin{k});
     end
     out=reshape(out,size(imin));dir=reshape(dir,size(imin));
 else
-    [out,dir]=asymmetryi(imin);
+    [out,asymmr,dir]=asymmetryi(imin);
 end
 end
-function [out,dir]=asymmetryi(imin)
+function [out,asymmr,dir]=asymmetryi(imin)
     if (ndims(imin)>2)
         for k=size(imin,3):-1:1
-            [out(k),dir(k)]=asymmetryi(imin(:,:,k));
+            [out(k),asymmr(k),dir(k)]=asymmetryi(imin(:,:,k));
         end
     else
-        [out,dir]=asymmetryi2(imin);
+        [out,asymmr,dir]=asymmetryi2(imin);
     end
 end
-function [out,dir]=asymmetryi2(imin)
+function [out,asymmr,dir]=asymmetryi2(imin)
 persistent X2 Y2 XY sx
 s=size(imin);
 if isempty(X2) || any(sx~=s)
     n=(0:s(1)-1)-s(1)/2+.5;
     [X,Y]=meshgrid(n,n);
-    X2=X.*X;
-    Y2=Y.*Y;
-    XY=X.*Y;
+    Xs=(X+Y)/2;
+    Ys=(-X+Y)/2;
+    X2=Xs.*Xs;
+    Y2=Ys.*Ys;
+    XY=Xs.*Ys;
     sx=size(X);
 end
 a11=sum(sum(X2.*imin));
@@ -34,7 +36,7 @@ a21=sum(sum(XY.*imin));
 a=[a11,a21;a21,a22];
 [e,v]=eig(a);
 out=abs((v(2,2)-v(1,1))/(v(2,2)+v(1,1)));
-
-    dir=atan2(e(2,1),e(1,1));
+asymmr=abs((a22-a11)/(a22+a11));
+    dir=mod(atan2(e(2,2),e(1,2)),pi)/pi*180;
 % end
 end
