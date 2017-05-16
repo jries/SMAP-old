f465ea6f56032eb3df3e43aab43599faf056ede9
ries <jonas.ries@gmail.com>
2017-05-16 11:33:03 +0200

    chekc bead calibration for any fit

--- plugins/+Analyze/+sr3D/CheckCalibration.m
+++ plugins/+Analyze/+sr3D/CheckCalibration.m
@@ -11,14 +11,30 @@ classdef CheckCalibration<interfaces.DialogProcessor
         end
 
         function out=run(obj,p)
-            locsROI=obj.locData.getloc({'frame','xnm','ynm','znm','phot'},'layer',1,'position','roi');
             locsall=obj.locData.getloc({'frame','xnm','ynm','znm','filenumber','phot'});
-%             plotzvsframe(locsROI,p)
-            beadlocs=getBeadLocs(locsROI,p);
-            maxd=p.cam_pixelsize_nm*2;
-            [locsall.beadnum,numlocs]=associatelocs(beadlocs.x,beadlocs.y,locsall.xnm,locsall.ynm,maxd);
+            maxfilen=max(locsall.filenumber);
+            
+            maxd=p.cam_pixelsize_nm(1);
+            try
+                p.refractiveIndexFactor=obj.locData.history{1}.children.fitparamters.fitterGUI.children.MLE_GPU_Yiming.refractive_index_mismatch;
+            catch
+                p.refractiveIndexFactor=1;
+            end
+            ax=obj.initaxis('filtered');
+            locsall.beadnum=zeros(size(locsall.xnm));
+              locsROI=obj.locData.getloc({'frame','xnm','ynm','znm','phot','filenumber'},'layer',1,'grouping','ungrouped','position','roi','removeFilter',{'filenumber'});
+            for k=1:maxfilen
+              
+                fileroi=locsROI.filenumber==k;
+                beadlocs=getBeadLocs(locsROI.xnm(fileroi),locsROI.ynm(fileroi),p);
+                infile=locsall.filenumber==k;
+                [bn,numlocs]=associatelocs(beadlocs.x,beadlocs.y,locsall.xnm(infile),locsall.ynm(infile),maxd);
+                goodnum=bn>0;
+                locsall.beadnum(infile)=bn+max(locsall.beadnum).*goodnum;
+            end
             plotmanybeads(locsall,p)
-
+%             plotzvsframe(locsROI,p)
+            
             out=0;
         end
         
@@ -32,73 +48,82 @@ end
 
 function plotmanybeads(locs,p)
 mmax=max(locs.beadnum);
-indf1=locs.filenumber==1;
-indf2=locs.filenumber==2;
+% indf1=locs.filenumber==1;
+% indf2=locs.filenumber==2;
 ax=initaxis(p.resultstabgroup,'zplots');
 axis(ax)
+hold off
 for k=1:mmax
     indb=locs.beadnum==k;
-    bf1=indb&indf1;
-    bf2=indb&indf2;
+    bf1=indb;
+%     bf2=indb&indf2;
     locsb1.frame=locs.frame(bf1);
     locsb1.znm=locs.znm(bf1);
     locsb1.phot=locs.phot(bf1);
     
-    locsb2.frame=locs.frame(bf2);
-    locsb2.znm=locs.znm(bf2);
-    locsb2.phot=locs.phot(bf2);
+%     locsb2.frame=locs.frame(bf2);
+%     locsb2.znm=locs.znm(bf2);
+%     locsb2.phot=locs.phot(bf2);
     pf=plotzvsframe(locsb1,p);
     slope1(k)=pf(1);off1(k)=-pf(2)/pf(1);
     hold on
-    pf=plotzvsframe(locsb2,p);
-    slope2(k)=pf(1);off2(k)=-pf(2)/pf(1);
-    hold off
-    
+%     pf=plotzvsframe(locsb2,p);
+%     slope2(k)=pf(1);off2(k)=-pf(2)/pf(1);
+%     hold off
+%     
     mx1(k)=mean(locs.xnm(bf1));
     my1(k)=mean(locs.ynm(bf1));
 %     drawnow
-%     waitforbuttonpress
+%      waitforbuttonpress
 end
 initaxis(p.resultstabgroup,'slope vs z')
-indb=abs(slope1)>1.3|abs(slope2)>2|abs(off1)>10000|abs(off2)>10000|abs(slope1)<.8;
-
+ indb=abs(slope1)>1.3|abs(off1)>10000|abs(slope1)<.8;
+indb=false(size(slope1));
 plot(off1(~indb),slope1(~indb),'ro')
-hold on
-plot(off2(~indb),slope2(~indb),'bx')
-hold off
+
 
 initaxis(p.resultstabgroup,'slope in image')
 scatter3(mx1(~indb),my1(~indb),slope1(~indb),[],slope1(~indb))
+xlabel('x position')
+ylabel('y position')
+zlabel('slope')
 end
 
 
 function pf=plotzvsframe(locs,p)
 
-diffz=50;
-refractiveIndexFactor=1.25;
+% diffz=10;
+% refractiveIndexFactor=1.25;
 
-z=locs.frame*diffz/refractiveIndexFactor;
-plot(z,locs.znm,'o')
+z=locs.frame*p.dz*p.refractiveIndexFactor;
+plot(z,locs.znm,'y.')
 % 
 % nd=7;
 % dz=locs.znm;
 % for k=1:nd
 %     dz=diff(dz);
 % end
- [~,ind]=max(locs.phot);
+fs=200/p.dz;
+findmax=filter(1/fs*ones(fs,1),1,locs.phot./(abs(locs.znm)+p.dz));
+ [~,ind]=max(findmax);
  
  hold on
-plot(z(ind),locs.znm(ind),'kd')
+% plot(z(ind),locs.znm(ind),'kd')
  
  
  
 %   [~,ind]=min(abs(dz))
-nd=0;
+% nd=0;
 
 
 dz1=diff(locs.znm);
-i2=find((dz1(ind:end)-50)<-100,1,'first')+ind-2;
-i1=find((dz1(1:ind-1)-50)<-100,1,'last')+1;
+
+dzf=filter(1/fs*ones(fs,1),1,dz1/p.refractiveIndexFactor/p.dz);
+i2=find((dzf(ind:end))<0.3|dzf(ind:end)>3,1,'first')+ind-2;
+i1=find((dzf(1:ind-1))<0.3|(dzf(1:ind-1))>3,1,'last')+1;
+
+% i2=find((dz1(ind:end))<-p.dz*3,1,'first')+ind-2;
+% i1=find((dz1(1:ind-1))<-p.dz*3,1,'last')+1;
 
 if isempty(i1)
     i1=1;
@@ -112,7 +137,7 @@ flin=z(i1:i2);
 pf=polyfit(flin,zlin,1);
 
 
-plot(flin,zlin,'x')
+plot(flin,zlin,'.')
 plot(flin,polyval(pf,flin),'r')
 hold off
 title(['slope: ' num2str(pf(1))])
@@ -122,7 +147,15 @@ function pard=guidef
 % pard.d3_color.object=struct('Style','checkbox','String','render in color');
 % pard.d3_color.position=[2,1];
 
-pard.text2.object=struct('String','check astigmatism calibration','Style','text');
-pard.text2.position=[2,1];
+pard.text2.object=struct('String','check bead calibration','Style','text');
+pard.text2.position=[1,1];
+
+pard.dzt.object=struct('String','dz (nm)','Style','text');
+pard.dzt.position=[2,1];
+
+pard.dz.object=struct('String','10','Style','edit');
+pard.dz.position=[2,2];
+
+pard.plugininfo.type='ProcessorPlugin';
 pard.plugininfo.type='ProcessorPlugin';
 end
