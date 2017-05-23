7b6e4cf8df3b28c668f6c4a069479bf9835321a8
ries <jonas.ries@gmail.com>
2017-05-23 18:34:21 +0200

    3d cal

--- plugins/+Analyze/+sr3D/private/getf0site.m
+++ plugins/+Analyze/+sr3D/private/getf0site.m
@@ -2,7 +2,11 @@ function [f0,PSFx0,PSFy0]=getf0site(locs,p)
 if strcmp(p.modality.selection,'astigmatic')
     
     p.ploton=0;
+    if isfield(locs,'znm')&&~isempty(locs.znm)
+         zas=getf0Z(locs,p);
+    else
     [zas,zn]=stackas2z(locs.PSFxpix,locs.PSFypix,locs.frame,locs.phot,p);
+    end
     if isnan(zas)
         PSFx0=NaN;
         PSFy0=NaN;
