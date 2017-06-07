11d933110f66ebcd4cd5fed4b433c5c5146e54ec
ries <jonas.ries@gmail.com>
2017-06-07 15:08:37 +0200

    dual color visualization

--- plugins/+Analyze/+cluster/density_calculator.m
+++ plugins/+Analyze/+cluster/density_calculator.m
@@ -14,48 +14,65 @@ classdef density_calculator<interfaces.DialogProcessor
             %obj.locData.sort('filenumber','channel','xnm');
             %problem on filtered data, somehow it gets confused.
             %[locs,indin]=obj.locData.getloc({'xnm','ynm','channel','frame','znm','ingrouped','inungrouped'},'position','all');
-           [locs,indin]=obj.locData.getloc({'xnm','ynm','filenumber','channel','frame','znm','ingrouped','inungrouped'},'layer',find(p.sr_layerson),'position','all');
-           
-            sortm=horzcat(locs.filenumber,locs.channel,locs.xnm,(1:length(locs.frame))');
-            [sortedm,sortind]=sortrows(sortm);
-            x=locs.xnm(sortind);
-            y=locs.ynm(sortind);
-            
-            dx=p.countingsize_xy;
-            dz=p.countingsize_z;
-            if ~isempty(locs.znm)
-                z=locs.znm(sortind);
-                if p.countingregion.Value==1 %Gauss
-                    neighbours=countneighbours3DGauss(double(x),double(y),double(z),double(dx),double(dz));
-                else
-                    neighbours=countneighbours3Dcirc(double(x),double(y),double(z),double(dx),double(dz));
+            if contains(p.countwhat.selection,'layer')
+                activelayers=find(p.sr_layerson);
+                for k=1:length(activelayers)
+                    fieldrem=setdiff(fieldnames(obj.locData.layer(k).filter),{'filenumber','channel'})';
+                    locall(k)=obj.locData.getloc({'xnm','ynm','filenumber','channel','frame','znm','ingrouped','inungrouped'},'layer',activelayers(k),'position','all','removeFilter',fieldrem,'grouping','ungrouped');
+                    locref(k)=obj.locData.getloc({'xnm','ynm','filenumber','channel','frame','znm','ingrouped','inungrouped'},'layer',activelayers(k),'position','all');
                 end
             else
-                if p.countingregion.Value==1 %Gauss
-                    neighbours=countneighbours2DGauss(double(x),double(y),double(dx));
-                else
-                    neighbours=countneighbours2Dcirc(double(x),double(y),double(dx));
-                end
+                locall=obj.locData.loc;
+                locref=obj.locData.loc;
             end
             
-            neighbourstot=zeros(length(obj.locData.loc.xnm),1);
-             [~,sortbackind]=sort(sortedm(:,4));
-             neighboursback=neighbours(sortbackind);
-             
             
-             
-            if sum(locs.inungrouped)==length(locs.xnm) %ungrouped data
-                 neighbourstot(locs.inungrouped)=neighboursback;
-            else
-%                 error('does not work at the moment for grouped data')
+            neighbourstotall=zeros(length(obj.locData.loc.xnm),1);
+%            [locs,indin]=obj.locData.getloc({'xnm','ynm','filenumber','channel','frame','znm','ingrouped','inungrouped'},'layer',find(p.sr_layerson),'position','all');
+            for lay=1:length(locall)
+                neighbourstot=zeros(length(obj.locData.loc.xnm),1);
+                sortmall=horzcat(locall(lay).filenumber,locall(lay).xnm,(1:length(locall(lay).frame))');
+                [sortedm,sortind]=sortrows(sortmall);
+                xa=locall(lay).xnm(sortind);
+                ya=locall(lay).ynm(sortind);
                 
-                neighbourstot=obj.locData.grouped2ungrouped(locs.ingrouped,neighboursback);
-                    
+                sortmallr=horzcat(locref(lay).filenumber,locref(lay).xnm,(1:length(locref(lay).frame))');
+                [sortedmr,sortindr]=sortrows(sortmallr);
+                xr=locref(lay).xnm(sortindr);
+                yr=locref(lay).ynm(sortindr);
+
+                dx=p.countingsize_xy;
+                dz=p.countingsize_z;
+                if ~isempty(locall(lay).znm)&&~isempty(locref(lay).znm)
+                    za=locall(lay).znm(sortind);
+                    zr=locref(lay).znm(sortindr);
+                    if p.countingregion.Value==1 %Gauss
+                        neighbours=countneighbours3DGauss(double(xa),double(ya),double(za),double(dx),double(dz));
+                        disp('not impolemented for Gauss')
+                    else
+                        neighbours=countneighbours3Dcirc2(double(xa),double(ya),double(za),double(xr),double(yr),double(zr),double(dx),double(dz));
+                    end
+                else
+                    if p.countingregion.Value==1 %Gauss
+                        neighbours=countneighbours2DGauss(double(xa),double(ya),double(dx));
+                        disp('not impolemented for Gauss')
+                    else
+                        neighbours=countneighbours2Dcirc2(double(xa),double(ya),double(xr),double(yr),double(dx));
+                    end
+                end
+                [~,sortbackind]=sort(sortedm(:,3));
+                neighboursback=neighbours(sortbackind);
+                if sum(locall(lay).inungrouped)==length(locall(lay).xnm) %ungrouped data
+                     neighbourstot(locall(lay).inungrouped)=neighboursback;
+                else
+                    nbbackug=obj.locData.grouped2ungrouped(locall(lay).ingrouped,neighboursback);
+                    neighbourstot=nbbackug;
+                end
+                neighbourstotall=max(neighbourstotall,neighbourstot);
             end
-           
             
-            
-            obj.locData.setloc('clusterdensity',single(neighbourstot));
+
+            obj.locData.setloc('clusterdensity',single(neighbourstotall));
 %             obj.locData.sort('filenumber','channel','frame');
             obj.locData.regroup;
             obj.setPar('locFields',fieldnames(obj.locData.loc));  
@@ -77,18 +94,24 @@ pard.countingregion.object.TooltipString=sprintf('count Gauss-weighted locs (mor
 pard.countingregion.position=[1,1];
 pard.countingregion.Width=2;
 
+pard.countwhat.object=struct('String','all|layers','Style','popupmenu','Value',2);
+pard.countwhat.object.TooltipString=sprintf('count all localizations or per layer (use visible ones as reference)');
+pard.countwhat.position=[2,1];
+pard.countwhat.Width=2;
+
+
 pard.texta.object=struct('String','size in x,y (nm)','Style','text');
-pard.texta.position=[2,1];
+pard.texta.position=[3,1];
 pard.countingsize_xy.object=struct('String','12','Style','edit');
-pard.countingsize_xy.position=[2,2];
+pard.countingsize_xy.position=[3,2];
 pard.countingsize_xy.isnumeric=1;
 pard.countingsize_xy.object.TooltipString=sprintf('radius of circle or sigma of gauss in lateral direction');
 
 
 pard.text1.object=struct('String','size in z (nm)','Style','text');
-pard.text1.position=[3,1];
+pard.text1.position=[4,1];
 pard.countingsize_z.object=struct('String','24','Style','edit');
-pard.countingsize_z.position=[3,2];
+pard.countingsize_z.position=[4,2];
 pard.countingsize_z.isnumeric=1;
 pard.countingsize_z.object.TooltipString=sprintf('size of cylinder or sigma of gauss in z direction');
 pard.plugininfo.name='density calculator (number of neighbours)';
--- plugins/+ROIManager/+Analyze/scripts/CME3DTrec.m
+++ plugins/+ROIManager/+Analyze/scripts/CME3DTrec.m
@@ -1,13 +1,15 @@
 %mammalian cells: temporal reconstruction
 %parameters
 p.cutoff=4; % for isosurface, at max(V(:))/cutoff
-p.sgauss=1;%smoothing of volume
+p.cutoffdc=3; % for isosurface, at max(V(:))/cutoff
+p.sgauss=2;%smoothing of volume
+p.sgauss2=.6;%smoothing of volume
 p.winsize=200; %plotting
 p.minx=0; %x: use 0 for central cut
-p.pixelsize=10; %nm, for volume reconstruction
-p.limxy=[0 40]; % for plotting, in volume pixels
-p.limz=[0 40];
-rot=0; % rotate sites to align bottom holes
+p.pixelsize=5; %nm, for volume reconstruction
+% p.limxy=[0 40]; % for plotting, in volume pixels
+% p.limz=[0 40];
+rot=1; % rotate sites to align bottom holes
 
 timewindows=20; % number of sites per average: (number of sites / this number)
 timepoints=100; % increment: (number of sites / this number)
@@ -20,7 +22,7 @@ sites=se.sites;
 % rSphere=getFieldAsVector(sites,'evaluation','CME3DDSpherefit','map3D','rSphere');
 mainFraction=getFieldAsVector(sites,'evaluation','CME3DDSpherefit','map3D','mainFraction');
 
-[~,indsort]=sort(mainFraction);
+[~,indsort]=sort(-mainFraction);
 
 sitessort=sites(indsort);
 % sitessort(1:10)=[];
@@ -29,7 +31,7 @@ mainFraction=getFieldAsVector(sitessort,'evaluation','CME3DDSpherefit','map3D','
 topcoverage=getFieldAsVector(sitessort,'evaluation','CME3DDSpherefit','map3D','topcoverage');
 thetac=getFieldAsVector(sitessort,'evaluation','CME3DDSpherefit','map3D','thetac');
 phic=getFieldAsVector(sitessort,'evaluation','CME3DDSpherefit','map3D','phic');
-
+thetacoverage=getFieldAsVector(sitessort,'evaluation','CME3DDSpherefit','map3D','bwfit','thetacoverage');
 
 dN=round(length(indsort)/timewindows);
 df=round(length(indsort)/timepoints);
@@ -42,23 +44,26 @@ rcorr=zeros(1,length(indsort));
 F(length(range))=struct('cdata',[],'colormap',[]);
 for k=1:length(range)
     numloc=0;
+    numloc2=0;
     
     indh=range(k):range(k)+dN;
     
     for s=indh
         numloc=numloc+length(sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.r);  
+        numloc2=numloc2+length(sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates2.x); 
     end
     
         
-
+    thcov=mean(pi-thetacoverage(indh));
     Rhere=mean(rSphere(indh));
         rmean(indh)=Rhere;
 
-    rcorr(indh)=rmean(indh)./rSphere(indh);
-    
-%     rh=zeros(1,numloc);rch=zeros(1,numloc);th=zeros(1,numloc);ph=zeros(1,numloc);
+    rcorr(indh)=rmean(indh)./rSphere(indh);   
+
     x=zeros(1,numloc);y=zeros(1,numloc);z=zeros(1,numloc);
-    ind=1;
+    x2=zeros(1,numloc2);y2=zeros(1,numloc2);z2=zeros(1,numloc2);
+    
+    ind=1;ind2=1;
     for s=indh
 %         if topcoverage(s)
 %             tcorr=0;
@@ -67,28 +72,36 @@ for k=1:length(range)
 %         end
 %         pcorr=0;
 % %         tcorr=-thetac(s);pcorr=-phic(s);
-if rot
-        xh=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.xc;
-        x(ind:ind+length(xh)-1)=xh*rcorr(s);
-        y(ind:ind+length(xh)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.yc*rcorr(s);
-        z(ind:ind+length(xh)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.zc*rcorr(s);
-%         x(ind:ind+length(r)-1)=r;
-%         rch(ind:ind+length(r)-1)=r*rcorr(s);
-%         th(ind:ind+length(r)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.t+tcorr;
-%         ph(ind:ind+length(r)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.p+pcorr;
+        if rot
+                xh=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.xc;
+                x(ind:ind+length(xh)-1)=xh*rcorr(s);
+                y(ind:ind+length(xh)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.yc*rcorr(s);
+                z(ind:ind+length(xh)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.zc*rcorr(s);
 
-else
-        xh=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.x;
-        x(ind:ind+length(xh)-1)=xh*rcorr(s);
-        y(ind:ind+length(xh)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.y*rcorr(s);
-        z(ind:ind+length(xh)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.z*rcorr(s);
-        
-end        
+                    xh2=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates2.xc;
+                    x2(ind2:ind2+length(xh2)-1)=xh2*rcorr(s);
+                    y2(ind2:ind2+length(xh2)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates2.yc*rcorr(s);
+                    z2(ind2:ind2+length(xh2)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates2.zc*rcorr(s);
+        %         x(ind:ind+length(r)-1)=r;
+        %         rch(ind:ind+length(r)-1)=r*rcorr(s);
+        %         th(ind:ind+length(r)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.t+tcorr;
+        %         ph(ind:ind+length(r)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.p+pcorr;
+
+        else
+                xh=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.x;
+                x(ind:ind+length(xh)-1)=xh*rcorr(s);
+                y(ind:ind+length(xh)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.y*rcorr(s);
+                z(ind:ind+length(xh)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates.z*rcorr(s);
+                    xh2=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates2.x;
+                    x2(ind2:ind2+length(xh2)-1)=xh2*rcorr(s);
+                    y2(ind2:ind2+length(xh2)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates2.y*rcorr(s);
+                    z2(ind2:ind2+length(xh2)-1)=sitessort(s).evaluation.CME3DDSpherefit.map3D.coordinates2.z*rcorr(s);        
+        end        
         ind=ind+length(xh);
-        
+        ind2=ind2+length(xh2);
         
     end
-    plotcoords(x,y,z,Rhere,p)
+    plotcoords(x,y,z,Rhere,thcov,p,x2,y2,z2)
     F(k)=getframe(gcf);
     
 
@@ -109,34 +122,74 @@ end
 
 
 
-function plotcoords(x,y,z,R,p)
+function plotcoords(x,y,z,R,thcov,p,x2,y2,z2)
 % figure(88)
 % scatter3(x,y,z+R,[],z+R)
 % axis equal
 % [t,p,r]=cart2sph(x,y,z);
 % figure(89)
 % polarplot(p,r,'.')
+dz=R*cos(thcov);
+xrange=[-p.minx,p.winsize];
+yrange=[-p.winsize,p.winsize];
+zrange=[-p.winsize,p.winsize]+p.winsize/2;
 
-V=myhist3(x,y,z+R,p.pixelsize,[-p.minx,p.winsize],[-p.winsize,p.winsize],[-p.winsize,p.winsize]+p.winsize/2);
+xr=xrange(1)+p.pixelsize:p.pixelsize:xrange(2)-p.pixelsize;
+yr=yrange(1)+p.pixelsize:p.pixelsize:yrange(2)-p.pixelsize;
+zr=zrange(1)+p.pixelsize:p.pixelsize:zrange(2)-p.pixelsize;
+V=myhist3(x,y,-(z+dz),p.pixelsize,xrange,yrange,zrange);
 Vs=smooth3(V,'gaussian',[1 1 1]*(round(2*p.sgauss/2)*2+1),p.sgauss);
 
-co=max(V(:))/p.cutoff;
+
+V2=myhist3(x2,y2,-(z2+dz),p.pixelsize,xrange,yrange,zrange);
+Vs2=smooth3(V2,'gaussian',[1 1 1]*(round(2*p.sgauss/2)*2+1),p.sgauss2);
+
+[X,Y,Z]=meshgrid(yr,xr,zr);
+
+co=max(Vs(:))/p.cutoff;
+codc=max(Vs(:))/p.cutoffdc;
 figure(90);
 clf
-hiso=patch(isosurface(Vs,co),'EdgeColor','none','FaceColor',[1,.75,.65]);
-isonormals(Vs,hiso);
+hiso=patch(isosurface(X,Y,Z,Vs,co),'EdgeColor','none','FaceColor',[1,.75,.65]);
+isonormals(X,Y,Z,Vs,hiso);
 
-hcap=patch(isocaps(Vs,co),'FaceColor','interp','EdgeColor','none');
+hcap=patch(isocaps(X,Y,Z,Vs,co),'FaceColor','interp','EdgeColor','none');
 axis equal
-view(35,30)
+view(-35,30)
 
-
-xlim(p.limxy)
-ylim(p.limxy)
-zlim(p.limz)
+xlim(yrange)
+ylim(xrange)
+zlim(zrange)
 
 lightangle(45,30);
 lighting gouraud
+
+figure(91);
+clf
+hiso=patch(isosurface(X,Y,Z,Vs,codc),'EdgeColor','none','FaceColor',[1,.75,.65],'FaceAlpha',0.75);
+isonormals(X,Y,Z,Vs,hiso);
+hold on
+if 0
+indin=x2>0;
+scatter3(y2(indin),x2(indin),-z2(indin)-dz,2,'k')
+else
+% codc=max(V2(:))/6;
+codc=.5;
+hiso2=patch(isosurface(X,Y,Z,Vs2,codc),'EdgeColor','none','FaceColor',[0,.75,1]);
+isonormals(X,Y,Z,Vs2,hiso2);
+hcap2=patch(isocaps(X,Y,Z,Vs2,co),'FaceColor','interp','EdgeColor','none');
+end
+
+axis equal
+view(30,-15)
+
+xlim(yrange)
+ylim(xrange)
+zlim(zrange)
+
+lightangle(45,0);
+lighting gouraud
+
 
 % indg=r~=0;
 % % [z,x]=pol2cart(p(indg),r(indg));
