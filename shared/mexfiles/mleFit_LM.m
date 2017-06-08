283d5968bd43bec149ae0b87987384b927e4ba96
Ries Lab <Ries Lab>
2017-06-08 16:02:57 +0200

    update GPUmleFit_LM

--- shared/mexfiles/mleFit_LM.m
+++ shared/mexfiles/mleFit_LM.m
@@ -50,5 +50,16 @@ end
 
 
 [P,CRLB,LogL]=allfitters{fitter}(varargin{:});
+%%
+% 
+% <latex>
+% \begin{tabular}{|c|c|} \hline
+% $n$ & $n!$ \\ \hline
+% 1 & 1 \\
+% 2 & 2 \\
+% 3 & 6 \\ \hline
+% \end{tabular}
+% </latex>
+% 
  clear(allfittersnames{fitter})
 
