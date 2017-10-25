bf2d1e9461ca8b3b4bfe5853979f066c3e034435
ries <jonas.ries@gmail.com>
2017-10-25 16:32:29 +0200

    fibril growth: manual analysis

--- plugins/+ROIManager/+Analyze/scripts/analyzeFibrilGrowth.m
+++ plugins/+ROIManager/+Analyze/scripts/analyzeFibrilGrowth.m
@@ -4,6 +4,7 @@ sites=se.sites;
 dfall=[];
 vall=[];
 lenall=[];
+timeall=[];
 for k=1:length(sites)
     try
         polyh=sites(k).evaluation.BALM_fibril_growth.poly;
@@ -15,7 +16,7 @@ for k=1:length(sites)
     dfall(end+1:end+l)=dpol(:,2)*frametime;
      lenall(end+1:end+l)=dpol(:,1)/1000;
     vall(end+1:end+l)=dpol(:,1)./dpol(:,2)/frametime/1000; %in um/s
-    
+    timeall(end+1:end+l)=polyh(1:end-1,2)*frametime;
 end
 
 maxwaitelongation=0.1; %in um
@@ -32,10 +33,19 @@ histogram(dfall(~stop),0:100:max(dfall(~stop)));
 title('growth time')
 xlabel('growth time s')
 
-% subplot(2,2,2);
-% histogram(vall);
+
 subplot(2,2,3);
 histogram(lenall(~stop),0:.010:max(lenall(~stop)));
 title('elongationstep')
 xlabel('elongationstep um')
+
+subplot(2,2,4);
+histogram(vall(~stop),0:1e-3:max(vall(~stop)));
+title('elongation speed')
+xlabel('elongation speed um/s')
+
+figure(100);
+plot(timeall(stop),dfall(stop),'+')
+
+
 
