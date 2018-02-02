c9c6ecc7249740f312ec985eecf5c370b57943db
ries <jonas.ries@gmail.com>
2018-02-02 12:36:03 +0100

    old saver useable

--- shared/savesml.m
+++ shared/savesml.m
@@ -53,13 +53,14 @@ if isfield(locData.files.file(1),'transformation')
     
 end
 
-if locData.getGlobalSetting('saveas73')
-    save(file,'saveloc','fileformat','parameters','-v7.3');
+if locData.getGlobalSetting('saveas73') %now I use this to save with the old saver: large files are saved as v7.3, not as small parts.
+    v=saverightversion(file,out,'-v7');
+    disp(['saved as version ' v])
+%     save(file,'saveloc','fileformat','parameters','-v7.3');
 else
     savematparts(file,out,{'saveloc','loc'});
-%     version='-v7';
+
 end
-% v=saverightversion(file,out,version);
-% disp(['saved as version ' v])
+
 % save(file,'saveloc','fileformat','parameters','-v7.3');
 end
