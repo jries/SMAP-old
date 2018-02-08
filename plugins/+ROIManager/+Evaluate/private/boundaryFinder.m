b56b678989011432aac6787e26afe3c70d73c57d
Yu-Le Wu <yu-le.wu@embl.de>
2018-02-08 17:43:11 +0100

    boundaryFinder was inplemented as a plug-in

--- plugins/+ROIManager/+Evaluate/private/boundaryFinder.m
+++ plugins/+ROIManager/+Evaluate/private/boundaryFinder.m
@@ -1,129 +1,147 @@
-% Import the kimograph
 global se
 sites=se.sites;
-test = sites(1).evaluation.BALM_fibril_growth.kimograph;
-test(test>=1)=1;
+for k = 1:length(sites)
+% Import the kimograph
+    test = sites(k).evaluation.BALM_fibril_growth.kimograph;
+    test(test>=1)=1;
 
-% Binarize the kimograph
-[testRefx, testRefy] = find(test);
-[testNZx, testNZy] = find(test);
+    slideStep = 5;
+    gridRange = 5;
+    adjM = 1.0003;
 
-% use euclidean distances to filter out noises
-ED = pdist2([testNZx testNZy],[testNZx testNZy],'euclidean');
-sortedED = sort(ED,2);
-sortedED = sortedED(:,1:20);
-rmean = mean(sortedED,2);
-test(sub2ind(size(test), testNZx([rmean>=5]), testNZy([rmean>=5]))) = 0;
+    % expand test
+    oriSize = size(test);
+    expand = zeros(oriSize(1),gridRange*slideStep);
+    test = [test, expand];
 
-% Get cordinates of the kemograph
-Size = size(test);
-[CorX CorY] = meshgrid(1:Size(1), 1:Size(2));
-Order = sub2ind(Size, CorX(:), CorY(:));
+    % Binarize the kimograph
+    [testRefx, testRefy] = find(test);
+    [testNZx, testNZy] = find(test);
 
-% Make grids
-gridRange = 5;
-adjCorX = ceil(CorX(:)./gridRange);
-adjCorY = ceil(CorY(:)./gridRange);
+    % use euclidean distances to filter out noises
+    K = round(length(testRefx)*5/100);
 
-% Get density
-D = accumarray([adjCorX adjCorY], test(Order), [], @sum);
+    ED = pdist2([testNZx testNZy],[testNZx testNZy],'euclidean');
+    sortedED = sort(ED,2);
+    sortedED = sortedED(:,1:K);
+    rmean = mean(sortedED,2);
 
-% Filter out grids that might be background
-cutoff = 3;
-D(D<=cutoff)=0;
+    EDcutoff = quantile(rmean, 0.95);
 
-% Do sliding subtraction
-SizeD = size(D);
-slideStep = 5;
+    test(sub2ind(size(test), testNZx([rmean>=EDcutoff]), testNZy([rmean>=EDcutoff]))) = 0;
 
-diff = cell(slideStep-1,1);
-diffAll = zeros(SizeD(1), SizeD(2)-slideStep);
-for i = 1:(slideStep-1)
-    diffAll = diffAll + (D(:,1+i:end-slideStep+i) - D(:,1:(end-slideStep)) < 0);
-end
+    % Get cordinates of the kemograph
+    Size = size(test);
+    [CorX CorY] = meshgrid(1:Size(1), 1:Size(2));
+    Order = sub2ind(Size, CorX(:), CorY(:));
 
-% Get grids with 4 zero grids on its right-hand side
-diffAllLog = diffAll == 4; 
-[bX,bY] = find(diffAllLog, 1, 'last');
+    % Make grids (see gridRange)
+    adjCorX = ceil(CorX(:)./gridRange);
+    adjCorY = ceil(CorY(:)./gridRange);
 
-% Get boundary (itself is 1, and its cumsum is also 1)
-boundary = diffAllLog & cumsum(diffAllLog,2,'reverse') == 1;
+    % Get density
+    D = accumarray([adjCorX adjCorY], test(Order), [], @sum);
 
-%figure(50)
-%mesh(boundary)
-%rotate3d on
+    % Filter out grids that might be background
+    cutoff = 3; %#Par
+    D(D<=cutoff)=0;
 
-% Find the corrdinates of boundary
-[Cx, Cy]= find(boundary,sum(boundary(:)),'last');
+    % Do sliding subtraction (see slideStep)
+    % to see the next slideStep-1 right neighbor is higher or not than the
+    % target
+    SizeD = size(D);
 
-% Make sure every frame (x-axies) have a point
-CxFinal = (1:75)';
-CyFinal = zeros(75,1);
-CyFinal(Cx) = Cy;
+    diff = cell(slideStep-1,1);
+    diffAll = zeros(SizeD(1), SizeD(2)-slideStep);
+    for i = 1:(slideStep-1)
+        diffAll = diffAll + (D(:,1+i:end-slideStep+i) - D(:,1:(end-slideStep)) < 0);
+    end
 
-% Find the cummax (next points should always >= the previous points)
-CyFinal = cummax(CyFinal);
-CxGridParent = CxFinal*gridRange;
-CyGridParent = CyFinal*gridRange;
+    % Get grids with 4 zero grids on its right-hand side
+    diffAllLog = diffAll == slideStep-1; 
+    [bX,bY] = find(diffAllLog, 1, 'last');
 
-meaningfulX = (CxGridParent+1)<=Size(1);
-CxGridParent = CxGridParent(meaningfulX);
-CyGridParent = CyGridParent(meaningfulX);
+    % Get boundary (itself is 1, and its cumsum is also 1)
+    boundary = diffAllLog & cumsum(diffAllLog,2,'reverse') == 1;
 
-CxFParent = (0:Size(1)-1)';
-CyFParent = zeros(Size(1),1);
-CyFParent(CxGridParent+1) = CyGridParent;
-CyFParent = cummax(CyFParent);
+    %figure(50)
+    %mesh(boundary)
+    %rotate3d on
+
+    % Find the corrdinates of boundary
+    [Cx, Cy]= find(boundary,sum(boundary(:)),'last');
+
+    % Make sure every frame (x-axies) have a point
+    bounSize = size(boundary);
+    CxFinal = (1:bounSize(1))';
+    CyFinal = zeros(bounSize(1),1);
+    CyFinal(Cx) = Cy;
+
+    % Find the cummax (next points should always >= the previous points)
+    CyFinal = cummax(CyFinal);
+    CxGridParent = CxFinal*gridRange;
+    CyGridParent = CyFinal*gridRange;
+
+    meaningfulX = (CxGridParent+1)<=Size(1);
+    CxGridParent = CxGridParent(meaningfulX);
+    CyGridParent = CyGridParent(meaningfulX);
+
+    CxFParent = (0:Size(1)-1)';
+    CyFParent = zeros(Size(1),1);
+    CyFParent(CxGridParent+1) = CyGridParent;
+    CyFParent = cummax(CyFParent);
 
 
+    figure(50)
+    scatter(testNZx, testNZy)
+    hold on
+    plot(CxFParent, CyFParent)
 
 
-figure(50)
-scatter(testNZx, testNZy)
-hold on
-plot(CxFParent, CyFParent)
+    Mref = calMeasurement(CxFParent,CyFParent,testNZx,testNZy,Size);
+    MrefO = Mref;
+    CyFParentA = CyFParent;
+    for i=size(CxFParent):-1:1
+       thisFrameY = testNZy(testNZx == CxFParent(i));
+       thisFrameY = thisFrameY(thisFrameY>CyFParent(i));
+       ii = 1;
+       while ii <= length(thisFrameY)
+           CyFParentO = CyFParentA;
 
+           % New boundary
+           CyFParentA(i)=thisFrameY(ii);
 
-Mref = calMeasurement(CxFParent,CyFParent,testNZx,testNZy,Size);
-MrefO = Mref;
-CyFParentA = CyFParent;
-for i=size(CxFParent):-1:1
-   thisFrameY = testNZy(testNZx == CxFParent(i));
-   thisFrameY = thisFrameY(thisFrameY>CyFParent(i));
-   ii = 1;
-   while ii <= length(thisFrameY)
-       CyFParentO = CyFParentA;
-       
-       % New boundary
-       CyFParentA(i)=thisFrameY(ii);
-       
-       CyFParentA = cummax(CyFParentA);
+           CyFParentA = cummax(CyFParentA);
 
-       % fit to new boundary
-       Mnew = calMeasurement(CxFParent,CyFParentA,testNZx,testNZy,Size);
-       if Mnew >= MrefO*1.0003 && Mnew >= Mref
-           MrefO = Mnew;
-           futherStep = 0;
-       else
-           if futherStep <= 5
-            CyFParentA = CyFParentO;
-            Mnew = MrefO;
-            futherStep = futherStep+1;
+           % fit to new boundary
+           Mnew = calMeasurement(CxFParent,CyFParentA,testNZx,testNZy,Size);
+           if Mnew >= MrefO*adjM && Mnew >= Mref %#Par
+               MrefO = Mnew;
+               futherStep = 0;
            else
-            CyFParentA = CyFParentO;
-            Mnew = MrefO;
-            break
+               if futherStep <= 5
+                CyFParentA = CyFParentO;
+                Mnew = MrefO;
+                futherStep = futherStep+1;
+               else
+                CyFParentA = CyFParentO;
+                Mnew = MrefO;
+                break
+               end
            end
+           ii=ii+1;
        end
-       ii=ii+1;
-   end
-figure(51)
-scatter(testNZx, testNZy)
-hold on
-plot(CxFParent, CyFParentA)
-hold off
+        if isinteger(i/10)
+            figure(51)
+            scatter(testNZx, testNZy)
+            hold on
+            plot(CxFParent, CyFParentA)
+            hold off
+        end
+    end
+    display(k)
+    g.locData.SE.sites(1).evaluation.BALM_fibril_growth = [CxFParent/10000 CyFParentA/10];
 end
-
 function M = calMeasurement1(x,y,qx,qy,Size)
     ref = interp1(x,y,qx);
     rightIdx = qy > ref;
