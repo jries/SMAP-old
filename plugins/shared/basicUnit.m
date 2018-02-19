529c58fba2787b9f89474aa451551569758c99cf
Yu-Le Wu <yu-le.wu@embl.de>
2018-02-19 18:58:50 +0100

    Develop the classes of basicUnit and proteinES

--- plugins/shared/basicUnit.m
+++ plugins/shared/basicUnit.m
@@ -1,4 +1,4 @@
-classdef basicUnit
+classdef basicUnit < handle
 
     properties
         %define class properties if needed
@@ -7,8 +7,9 @@ classdef basicUnit
         bottomDepth
         bottomShape
         radius
+        totalDepth
         basicComponent
-        
+        basicComponent3D
     end
     methods
         function obj=basicUnit(capDepth, capShape, bottomDepth, bottomShape, radius)
@@ -17,8 +18,52 @@ classdef basicUnit
             obj.capShape = capShape;
             obj.radius = radius;
             obj.bottomDepth = bottomDepth;
+            obj.totalDepth = capDepth + bottomDepth;
             obj.bottomShape = bottomShape;
             obj.basicComponent = mkBasicCom(capDepth, capShape, bottomDepth, bottomShape, radius);
+            [x,y,z] = cylinder(obj.basicComponent.x, 1000);
+            basicComponent3D = table(x(:),y(:),z(:)*obj.totalDepth);
+            basicComponent3D.Properties.VariableNames = {'x' 'y' 'z'};
+            obj.basicComponent3D = basicComponent3D;
+        end
+        
+        function plot(obj, mode)
+            switch mode
+                case '2d'
+                    plot([obj.basicComponent.x -obj.basicComponent.x(end:-1:1)], [obj.basicComponent.y obj.basicComponent.y(end:-1:1)])
+                case '3d'
+                    scatter3(obj.basicComponent3D.x, obj.basicComponent3D.y, obj.basicComponent3D.z)
+                case '3dPx'
+                    scatter3(obj.basicComponent3DPx.x, obj.basicComponent3DPx.y, obj.basicComponent3DPx.z)
+            end
+        end
+        
+        function time = getTime(obj, v, t)
+            time = obj.basicComponent3DPx;
+            time.z = obj.basicComponent3DPx.z - obj.totalDepth + v * t;
+        end
+        
+        function fullPx = fullPixel(obj)
+            [x,y,z] = meshgrid((-obj.radius):obj.radius, (-obj.radius):obj.radius, 0:obj.totalDepth);
+            fullPx = table(x(:),y(:),z(:));
+            fullPx.Properties.VariableNames = {'x' 'y' 'z'};
+        end
+    end
+    methods(Static)
+        function  keept = pointsInCy(X, Y, Z, x, y, z)
+            IdxXR = X >= 0;
+            IdxXL = X <= 0;
+            xRefR = griddata(Y(IdxXR),Z(IdxXR),X(IdxXR), y, z, 'cubic');
+            xRefL = griddata(Y(IdxXL),Z(IdxXL),X(IdxXL), y, z, 'cubic');
+            IdxYR = Y >= 0;
+            IdxYL = Y <= 0;
+            yRefR = griddata(X(IdxYR),Z(IdxYR),Y(IdxYR), x, z, 'cubic');
+            yRefL = griddata(X(IdxYL),Z(IdxYL),Y(IdxYL), x, z, 'cubic');
+            keeptByx = x <= xRefR & x >= xRefL ;
+            keeptByy = y <= yRefR & y >= yRefL ;
+            keeptByy(y<5&y>-5)=keeptByx(y<5&y>-5);
+            keeptByx(x<5&x>-5)=keeptByy(x<5&x>-5);
+            keept = keeptByx;
         end
     end
 end
@@ -34,6 +79,8 @@ function  basicComponent = mkBasicCom(capDepth, capShape, bottomDepth, bottomSha
         t=-pi:0.01:pi;
         x=xO+a*cos(t);
         y=yO+b*sin(t);
+        y(y == max(y)) = round(max(y));
+        x(y == max(y)) = 0;
         idxKept = x<=xO&y>=yO;
         cx = x(idxKept);
         cy = y(idxKept);
@@ -54,7 +101,8 @@ function  basicComponent = mkBasicCom(capDepth, capShape, bottomDepth, bottomSha
     basicComponent = [];
     xy = unique([x;y]','rows','stable');
     yq = 0:(capDepth+bottomDepth);
-    xq = interp1(xy(:,2),xy(:,1),yq);
+    xq = interp1(xy(:,2),xy(:,1),yq,'cubic');
     basicComponent.x = xq;
     basicComponent.y = yq;
 end
+
--- plugins/shared/proteinES.m
+++ plugins/shared/proteinES.m
@@ -1,22 +1,77 @@
-classdef proteinES
-
+classdef proteinES < handle
+    % pes = proteinES('121212', 'sla1', bu, 'ellipse','bar')
+    % pes.addTimePoint(table([1],[1],[10],[20],[5],[5],[200],{'bar'},'VariableNames',{'time', 'dz', 'th', 'de', 'zm', 'shi', 'nm', 'sha'}));
     properties
         %define class properties if needed
         proteinID
         proteinName
-        par
+        endocyticPM
+        timePar
+        capShape
+        bottomShape
+        timeData
     end
     methods
-        function obj=proteinES(proteinID, proteinName)   %replace by filename        
+        function obj=proteinES(proteinID, proteinName, basicUnit, capShape, bottomShape)   %replace by filename        
             obj.proteinID = proteinID;
             obj.proteinName = proteinName;
-            par = table([],[],[],[],[],[],[],[],'VariableNames',{'time', 'dz', 'th', 'de', 'ym', 'shi', 'nm', 'sha'});
-            obj.par = par;
+            obj.endocyticPM = basicUnit;
+            obj.capShape = capShape;
+            obj.bottomShape = bottomShape;
+            timePar = table([],[],[],[],[],[],[],[],'VariableNames',{'time', 'dz', 'th', 'de', 'zm', 'shi', 'nm', 'sha'});
+            % obj.timePar = table([1],[1],[10],[20],[5],[5],[200],{'bar'},'VariableNames',{'time', 'dz', 'th', 'de', 'zm', 'shi', 'nm', 'sha'});
+            obj.timePar = timePar;
+            obj.timeData = [];
         end
-        function getTimeImg(t)
+        function addTimePoint(obj, aTable)
+            obj.timePar = [obj.timePar;aTable];
+        end
+        function img = getTimeImg(obj, t, viewType, imageSize)
+            if isempty(obj.timeData.(['t' int2str(t)])) 
+                obj.timeData.(['t' t]) = obj.getTime(t);
+            end    
+            switch viewType
+                case 'top'
+                    % accumulate all the even points in the space to z=1
+                    idx = int16([obj.timeData.(['t' int2str(t)]).x obj.timeData.(['t' int2str(t)]).y])+imageSize/2;
+                    idxZ = obj.timeData.(['t' int2str(t)]).z;
+                    proImg = accumarray(idx, idxZ,[],@(x) length(x));
+                    proImg = [proImg zeros(min(size(proImg)),imageSize-min(size(proImg)))];
+                    img = [proImg; zeros(imageSize-min(size(proImg)),imageSize)];
+
+                    figure(31)
+                    image(img, 'CDataMapping', 'scaled')
+                    % imwrite(uint8(round(mat2gray(proImg)*255)), [p.folderPath '\' p.imgPath])
+                case 'side'
+                    % accumulate all the even points in the space to z=1
+                    idx = int16([obj.timeData.(['t' int2str(t)]).x+imageSize/2 obj.timeData.(['t' int2str(t)]).z+1]);
+                    idxZ = obj.timeData.(['t' int2str(t)]).y+imageSize/2;
+                    proImg = accumarray(idx, idxZ,[],@(x) length(x));
+                    Size = size(proImg);
+                    proImg = [proImg; zeros(imageSize-Size(1),Size(2))];
+                    Size = size(proImg);
+                    img = [proImg zeros(imageSize, imageSize-Size(2))];
+                    figure(31)
+                    image(img', 'CDataMapping', 'scaled')
+                    % imwrite(uint8(round(mat2gray(proImg')*255)), [p.folderPath '\' p.imgPath])
+            end
+        end
+        function time = getTime(obj, t)
+            % timeTime = getTime(pes,1);
+            thisTP = obj.timePar(obj.timePar.time==t,:);
+            inner = basicUnit(obj.endocyticPM.capDepth+thisTP.shi, obj.endocyticPM.capShape, obj.endocyticPM.bottomDepth, obj.endocyticPM.bottomShape, obj.endocyticPM.radius+thisTP.shi);
             
+            if strcmp(obj.endocyticPM.capShape, obj.capShape) && strcmp(obj.endocyticPM.bottomShape, obj.bottomShape) % Check the shapes of inner and outer columns are the same or not
+                outer = basicUnit(obj.endocyticPM.capDepth+thisTP.shi+thisTP.th, obj.endocyticPM.capShape, obj.endocyticPM.bottomDepth, obj.endocyticPM.bottomShape, obj.endocyticPM.radius+thisTP.shi+thisTP.th);
+            end
+            
+            Px = fullPixel(outer);
+            inOuter = basicUnit.pointsInCy(outer.basicComponent3D.x, outer.basicComponent3D.y, outer.basicComponent3D.z, Px.x(:), Px.y(:), Px.z(:));
+            Px = Px(inOuter,:);
+            inInner = basicUnit.pointsInCy(inner.basicComponent3D.x, inner.basicComponent3D.y, inner.basicComponent3D.z, Px.x(:), Px.y(:), Px.z(:));
+            Px = Px(~inInner,:);
+            thisBasicComponent3DPx = Px;
+            time = thisBasicComponent3DPx(thisBasicComponent3DPx.z <= thisTP.zm+thisTP.de/2 & thisBasicComponent3DPx.z >= thisTP.zm-thisTP.de/2 ,:);
         end
     end
 end
-
-
