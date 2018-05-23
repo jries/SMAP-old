62f9812844babe9ca3cafa7227dd5d1962203ff3
ries <jonas.ries@gmail.com>
2018-05-23 11:36:30 +0200

    now use same saveloc for fitter ands SMLsaver. allows to use load gui parameters to restore fitting workflow, if correct workflow has been loaded

--- shared/savesml.m
+++ shared/savesml.m
@@ -15,52 +15,60 @@ if p.saveroi
     indg=indgl&indgroi;
     
 else
-    indg=true(size(locData.loc.xnm));
+%     indg=true(size(locData.loc.xnm));
+    indg=[];
 end
 
 if isfield(p,'savefile') && p.savefile
     filenumber=p.dataselect.Value;
-    indg=indg&locData.loc.filenumber==filenumber;
-end
-if all(indg)%save all
-    indg=[];
-end
-if nargin>3
-locData.loc=rmfield(locData.loc,excludesavefields);
-end
-saveloc=locData.savelocs([],indg); % BETA , maybe problematic with more than 1 file: this will save only displayed loicalizations
-% if ~isempty(locData.SE)
-%     saveloc.siteexplorer=locData.SE.save;
-% end
-if isfield(p,'savefile') && p.savefile
-    saveloc.file=saveloc.file(filenumber);
-    saveloc.history=saveloc.history(filenumber);
-    saveloc.loc.filenumber=ones(size(locData.loc.filenumber));
-end
-
-
-rg=p.mainGui; 
-parameters=rg.saveParameters;
-fileformat.name='sml';
-out=struct('saveloc',saveloc,'fileformat',fileformat,'parameters',parameters);
-if isfield(locData.files.file(1),'transformation')
-    for k=length(locData.files.file):-1:1
-        if ~isempty(locData.files.file(k).transformation)
-            out.transformation=locData.files.file(k).transformation;
-            break
-        end
-    end
-    
-end
-
-if locData.getGlobalSetting('saveas73') %now I use this to save with the old saver: large files are saved as v7.3, not as small parts.
-    v=saverightversion(file,out,'-v7');
-    disp(['saved as version ' v])
-%     save(file,'saveloc','fileformat','parameters','-v7.3');
+%     indg=indg&locData.loc.filenumber==filenumber;
 else
-    savematparts(file,out,{'saveloc','loc'});
-
+    filenumber=[];
 end
+
+% if all(indg)%save all
+%     indg=[];
+% end
+
+
+
+% if nargin>3
+% locData.loc=rmfield(locData.loc,excludesavefields);
+% end
+% 
+% if isfield(p,'savefile') && p.savefile
+%     saveloc.file=saveloc.file(filenumber);
+%     saveloc.history=saveloc.history(filenumber);
+%     saveloc.loc.filenumber=ones(size(locData.loc.filenumber));
+% end
+
+
+saveloc=locData.savelocs(file,indg,[],[],excludesavefields,filenumber); % BETA , maybe problematic with more than 1 file: this will save only displayed loicalizations
+
+
+
+% rg=p.mainGui; 
+% parameters=rg.saveParameters;
+% fileformat.name='sml';
+% out=struct('saveloc',saveloc,'fileformat',fileformat,'parameters',parameters);
+% if isfield(locData.files.file(1),'transformation')
+%     for k=length(locData.files.file):-1:1
+%         if ~isempty(locData.files.file(k).transformation)
+%             out.transformation=locData.files.file(k).transformation;
+%             break
+%         end
+%     end
+%     
+% end
+% 
+% if locData.getGlobalSetting('saveas73') %now I use this to save with the old saver: large files are saved as v7.3, not as small parts.
+%     v=saverightversion(file,out,'-v7');
+%     disp(['saved as version ' v])
+% %     save(file,'saveloc','fileformat','parameters','-v7.3');
+% else
+%     savematparts(file,out,{'saveloc','loc'});
+% 
+% end
 
 % save(file,'saveloc','fileformat','parameters','-v7.3');
 end
